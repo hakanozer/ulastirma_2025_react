b2f91142578da65a9b8addd209b76de14c3b5d54
Hakan ÖZER <hakanozer02@gmail.com>
2025-10-02 14:29:26 +0300

    Likes

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -2,17 +2,20 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { getOneProduct } from '../services/productService'
 import { IProduct } from '../models/IAllProducts'
+import { addRemoveLike, fncIsLike } from '../utils/likesStore'
 
 function ProductDetail() {
 
   const params = useParams()
+  const id = Number(params.id)
   const navigate = useNavigate()
   const [item, setItem] = useState<IProduct>()
   const [bigImage, setBigImage] = useState('')
   const [isLike, setIsLike] = useState(false)
 
   useEffect(() => {
-    const id = Number(params.id)
+    const isStatus = fncIsLike(id)
+    setIsLike(isStatus)
     if (Number.isInteger(id) && id > -1) {
         getOneProduct(id).then(res => {
             const dt = res.data
@@ -27,6 +30,11 @@ function ProductDetail() {
   }, [])
   
 
+  const addRemove = () => {
+    setIsLike(!isLike)
+    addRemoveLike(id)
+  }
+
   return (
     <>
     { item &&
@@ -36,7 +44,7 @@ function ProductDetail() {
                 <h2>{item.title}</h2>
                 <h3>{item.price}₺</h3>
                 <p><span className="badge text-bg-warning">{item.category}</span></p>
-                <i onClick={() => setIsLike(!isLike)} className={isLike ? 'bi bi-hand-thumbs-up-fill fs-3 text-danger' : 'bi bi-hand-thumbs-up fs-3 text-danger'}></i>
+                <i onClick={() => addRemove() } className={isLike ? 'bi bi-hand-thumbs-up-fill fs-3 text-danger' : 'bi bi-hand-thumbs-up fs-3 text-danger'}></i>
                 <p>{item.description}</p>
             </div>
             <div className='col-sm-6'>
---
+++ src/utils/likesStore.ts
@@ -0,0 +1,33 @@
+export const addRemoveLike = (id: number) => {
+    // likes control
+    const stLikes = localStorage.getItem('likes')
+    if(stLikes) {
+        // dizi daha önceden var
+        const arr = JSON.parse(stLikes) as number[]
+        const index = arr.findIndex((item) => item === id)
+        if (index === -1) {
+            // yok - ekle
+            arr.push(id)
+        }else {
+            // var - çıkar
+            arr.splice(index, 1)
+        }
+        const stArr = JSON.stringify(arr)
+        localStorage.setItem('likes', stArr.toString())
+    }else {
+        // dizi ilk kayıt
+        const likeArr = [id]
+        const stArr = JSON.stringify(likeArr)
+        localStorage.setItem('likes', stArr.toString())
+    }
+}
+
+export const fncIsLike = (id: number) => {
+    const stLikes = localStorage.getItem('likes')
+    if (stLikes) {
+        const arr = JSON.parse(stLikes) as number[]
+        const index = arr.findIndex((item) => item === id)
+        return index > -1 
+    }
+    return false
+}
